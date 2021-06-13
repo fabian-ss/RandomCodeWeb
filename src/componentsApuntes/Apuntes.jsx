1bc579880cbef1f3bb951cf17f50fe2676acafaa
Fabian <jsolteroe1300@alumno.ipn.mx>
2021-06-13 03:03:36 -0500

    s

--- src/componentsApuntes/Apuntes.jsx
+++ src/componentsApuntes/Apuntes.jsx
@@ -4,56 +4,64 @@ import '../styles/StyleApuntes.css';
 function Apuntes() {
     return (
         <Fragment>
-        <div>
-            
-        <div className="main-cuerpo-global1">
+            <div>
 
-<div className="main-cuerpo-barralateral-izq">
+                <div className="main-cuerpo-global1">
 
-         <div className="main-cuerpo-barralateral-izq-interno">
-         
-             <h1>Menu</h1>
-             <div className="linea"></div>
-             <a href="#">uno</a>
-             <a href="#">dos</a>
-             <a href="#">tres</a>
-             <a href="#">cuatro</a>
-             <a href="#">cinco</a>
+                    <div className="main-cuerpo-barralateral-izq">
 
+                        <div className="main-cuerpo-barralateral-izq-interno">
 
-         </div>
+                            <h1>Menu</h1>
+                            <div className="linea"></div>
+                            <a href="#AputeTema">uno</a>
+                            <a href="#ApunteSubtema1">dos</a>
+                            <a href="#ApunteSubtema2">tres</a>
+                            <a href="#ApunteSubtema3">cuatro</a>
+                            <a href="#ApunteSubtema4">cinco</a>
 
 
- </div> 
-
- <div className="main-cuerpo-superior1">
-
-     <div className="main-cuerpo-superior-der-interno">
-
-         <h1>Materia</h1>
-
-         <h2>Tema</h2>
-
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
-
-         <h3>Subtema</h3>
-
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
-
-         <h3>Subtema</h3>
-
-         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
-         
-         
-     </div>
-
- </div>
+                        </div>
 
 
-</div>
+                    </div>
+
+                    <div className="main-cuerpo-superior1">
+
+                        <div className="main-cuerpo-superior-der-interno">
+
+                            <h1>Materia</h1>
+
+                            <h2 id="#AputeTema">Tema</h2>
+
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
+
+                            <h3 id="#ApunteSubtema1">Subtema1</h3>
+
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
+
+                            <h3 id="#ApunteSubtema2">Subtema2</h3>
+
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
+                            
+                            <h3 id="#ApunteSubtema3">Subtema3</h3>
+
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
+
+                            <h3 id="#ApunteSubtema4">Subtema4</h3>
+
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aut pariatur, nobis consequatur debitis necessitatibus porro non rem? Nisi minus tempora sequi quis aliquid adipisci amet illum natus facere aut dolore, velit beatae temporibus qui similique quo odio cumque blanditiis quos laudantium sint provident quod nihil! Explicabo quis reiciendis sapiente doloremque eos ad, praesentium rem aliquid ipsam placeat magnam! Error quam dolorem enim atque ad repudiandae at eum nemo, dignissimos laborum consectetur pariatur aperiam reprehenderit facilis doloribus tempora harum quidem nam alias nostrum laudantium blanditiis aut. Voluptatem eligendi, non totam, architecto consequatur, dignissimos tempora cumque dolorem a soluta labore nulla.</p>
 
 
-        </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
         </Fragment>
     )
 }
